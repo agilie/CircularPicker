f9126f53a05ac1fe077b9e449dcf8fb6c3ccab91
Roman Kapshuk <roman.kapshuk@agilie.com>
2017-06-21 18:11:32 +0300

    Example update

--- TimePickerExample/src/main/java/com/agilie/agtimepickerexample/MainActivity.kt
+++ TimePickerExample/src/main/java/com/agilie/agtimepickerexample/MainActivity.kt
@@ -18,39 +18,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         view_pager.apply {
-
             pageMargin = -50
             clipChildren = false
             setPageTransformer(false, PickerPagerTransformer(context))
         }
 
-        view_pager.onAddView(TimePickerView(this).PickerBehavior()
-                .setGradient(intArrayOf(
-                        Color.parseColor("#00EDE9"),
-                        Color.parseColor("#0087D9"),
-                        Color.parseColor("#8A1CC3")), 45)
-                .setMaxLap(2)
-                .setMaxValue(24)
-                .setValueChangeListener(object : TimePickerContract.Behavior.ValueChangeListener {
-                    override fun onValueChanged(value: Int) {
-                        Log.d("valTest", "Hour $value \n" +
-                                "__________________________")
-                    }
-                }).build())
+        view_pager.onAddView(TimePickerView(this).apply {
+            colors = (intArrayOf(
+                    Color.parseColor("#00EDE9"),
+                    Color.parseColor("#0087D9"),
+                    Color.parseColor("#8A1CC3")))
+            gradientAngle = 220
+            maxLapCount = 2
+            maxValue = 24
+            centeredTextSize = 60f
+            centeredText = "Hours"
+            valueChangeListener = (object : TimePickerContract.Behavior.ValueChangeListener {
+                override fun onValueChanged(value: Int) {
+                    Log.d("valTest", "Hour $value \n" +
+                            "__________________________")
+                }
+            })
+        })
 
-        view_pager.onAddView(TimePickerView(this).PickerBehavior()
-                .setGradient(intArrayOf(
-                        Color.parseColor("#FF8D00"),
-                        Color.parseColor("#FF0058"),
-                        Color.parseColor("#920084")), 45)
-                .setMaxValue(60)
-                .setMaxLap(1)
-                .setValueChangeListener(object : TimePickerContract.Behavior.ValueChangeListener {
-                    override fun onValueChanged(value: Int) {
-                        Log.d("valTest", "Hour $value \n" +
-                                "__________________________")
-                    }
-                }).build())
+        view_pager.onAddView(TimePickerView(this).apply {
+            colors = (intArrayOf(
+                    Color.parseColor("#FF8D00"),
+                    Color.parseColor("#FF0058"),
+                    Color.parseColor("#920084")))
+            gradientAngle = 150
+            maxValue = 60
+            maxLapCount = 1
+            centeredTextSize = 60f
+            centeredText = "Minutes"
+            valueChangeListener = object : TimePickerContract.Behavior.ValueChangeListener {
+                override fun onValueChanged(value: Int) {
+                    Log.d("valTest", "Minutes $value \n" +
+                            "__________________________")
+                }
+            }
+        })
 
 
     }
